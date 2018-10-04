845ffc13c682e0b28f18bf5e50c2916f0a84b4f3
Laura Masselos <s1603459@sms.ed.ac.uk>
2018-10-04 16:48:35 +0100

    testing as I go

---
+++ tests/parseTest.c
@@ -0,0 +1,34 @@
+#include "minic-stdlib.h"
+
+/*
+structdecl ::= "struct" IDENT { (vardecl)+ };
+
+vardecl ::= type IDENT ; | type IDENT [ INT_LITERAL ];
+
+fundecl ::= type IDENT ( params) block
+*/
+// tested; structdecl function works!
+struct struct1 {
+  // tested; vardecl function works!
+  int a;
+  char* str[15];
+  void main;
+  struct s str;
+};
+
+
+struct struct2 {
+  // tested; vardecl function works!
+  int a;
+  char* str[15];
+  void main;
+  struct no u;
+};
+
+int* n;
+char* s1[90];
+
+void n (int a, int* b) {
+
+  int* c; void d; char e[5]; struct f g;
+}
