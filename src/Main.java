b6c8019d452706bcf5c62d878da4c6262c32bf2e
s1603459 <s1603459@sms.ed.ac.uk>
2018-10-28 22:16:29 +0000

    Revert "Merge https://git.ecdf.ed.ac.uk/cdubach/ct-18-19"
    This reverts commit 55816500871c5ee3fd50d46e21d482321de23fd1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import ast.ASTPrinter;
 import ast.Program;
-import gen.CodeGenerator;
 import lexer.Scanner;
 import lexer.Token;
 import lexer.Tokeniser;
@@ -115,21 +114,7 @@ public class Main {
             } else
                 System.exit(PARSER_FAIL);
         } else if (mode == Mode.GEN) {
-            Parser parser = new Parser(tokeniser);
-            Program programAst = parser.parse();
-            if (parser.getErrorCount() > 0)
-                System.exit(PARSER_FAIL);
-            SemanticAnalyzer sem = new SemanticAnalyzer();
-            int errors = sem.analyze(programAst);
-            if (errors > 0)
-                System.exit(SEM_FAIL);
-            CodeGenerator codegen = new CodeGenerator();
-            try {
-                codegen.emitProgram(programAst, outputFile);
-            } catch (FileNotFoundException e) {
-                System.out.println("File "+outputFile.toString()+" does not exist.");
-                System.exit(FILE_NOT_FOUND);
-            }
+            System.exit(MODE_FAIL);
         } else {
         	System.exit(MODE_FAIL);
         }
